6533a98ba00f1722427ee3d43ab1cae1f2be4e49
Benjamin Graf <benjamin@eende.net>
2020-11-13 16:27:18 +0100

    SIM7000 secure: connection to GPRS, getting an IP address

--- src/TinyGsmClientSIM7000.h
+++ src/TinyGsmClientSIM7000.h
@@ -118,27 +118,7 @@ class TinyGsmSim7000 : public TinyGsmModem<TinyGsmSim7000>,
    * Inner Secure Client
    */
 
-  /*TODO(?))
-  class GsmClientSecureSIM7000 : public GsmClientSim7000
-  {
-  public:
-    GsmClientSecure() {}
-
-    GsmClientSecure(TinyGsmSim7000& modem, uint8_t mux = 0)
-     : public GsmClient(modem, mux)
-    {}
-
-  public:
-    int connect(const char* host, uint16_t port, int timeout_s) override {
-      stop();
-      TINY_GSM_YIELD();
-      rx.clear();
-      sock_connected = at->modemConnect(host, port, mux, true, timeout_s);
-      return sock_connected;
-    }
-    TINY_GSM_CLIENT_CONNECT_OVERRIDES
-  };
-  */
+  // See TinyGsmClientSIM7000Secure.h
 
   /*
    * Constructor
@@ -293,7 +273,7 @@ class TinyGsmSim7000 : public TinyGsmModem<TinyGsmSim7000>,
     return res;
   }
 
-  String getLocalIPImpl() {
+  virtual String getLocalIPImpl() {
     sendAT(GF("+CIFSR;E0"));
     String res;
     if (waitResponse(10000L, res) != 1) { return ""; }
@@ -307,7 +287,7 @@ class TinyGsmSim7000 : public TinyGsmModem<TinyGsmSim7000>,
    * GPRS functions
    */
  protected:
-  bool gprsConnectImpl(const char* apn, const char* user = NULL,
+  virtual bool gprsConnectImpl(const char* apn, const char* user = NULL,
                        const char* pwd = NULL) {
     gprsDisconnect();
 
@@ -376,7 +356,7 @@ class TinyGsmSim7000 : public TinyGsmModem<TinyGsmSim7000>,
     return true;
   }
 
-  bool gprsDisconnectImpl() {
+  virtual bool gprsDisconnectImpl() {
     // Shut the TCP/IP connection
     // CIPSHUT will close *all* open connections
     sendAT(GF("+CIPSHUT"));
--- src/TinyGsmClientSIM7000Secure.h
+++ src/TinyGsmClientSIM7000Secure.h
@@ -8,7 +8,7 @@ class TinyGsmSim7000Secure : public TinyGsmSim7000, public TinyGsmSSL<TinyGsmSim
   friend class TinyGsmSSL<TinyGsmSim7000Secure>;
 
   public:
-    explicit TinyGsmSim7000Secure(Stream& stream) : TinyGsmSim7000(stream) {}
+    explicit TinyGsmSim7000Secure(Stream& stream) : TinyGsmSim7000(stream) {};
 
     class GsmClientSecureSim7000 : public GsmClientSim7000 {
       public:
@@ -16,6 +16,36 @@ class TinyGsmSim7000Secure : public TinyGsmSim7000, public TinyGsmSSL<TinyGsmSim
 
         GsmClientSecureSim7000(TinyGsmSim7000Secure& modem, uint8_t mux = 0) : GsmClientSim7000(modem, mux) {}
     };
+
+  protected:
+    bool gprsConnectImpl(const char* apn, const char* user = NULL, const char* pwd = NULL) override {
+      gprsDisconnect();
+
+      sendAT(GF("+CNACT=1,\""), apn, "\"");
+      if (waitResponse(60000L) != 1) { return false; }
+
+      return true;
+    }
+
+    bool gprsDisconnectImpl() override {
+      sendAT(GF("+CNACT=0"));
+      if (waitResponse(60000L) != 1) { return false; }
+
+      return true;
+    }
+
+    String getLocalIPImpl() override {
+      sendAT(GF("+CNACT?"));
+
+      if (waitResponse(GF("+CNACT:")) != 1) { return ""; }
+
+      // Get the IP which is enclosed in '"'
+      String res;
+      res = stream.readStringUntil('\"');
+      res = stream.readStringUntil('\"');
+
+      return res;
+    }
 };
 
 #endif  // SRC_TINYGSMCLIENTSIM7000SECURE_H_
